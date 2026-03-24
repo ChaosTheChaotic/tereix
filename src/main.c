76c94909b8da4c3c40e088b377ac563e70aaed6a
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2026-03-24 22:06:59 +0000

    Impl parser

--- src/main.c
+++ src/main.c
@@ -11,9 +11,14 @@
 #define AS_FLOAT(n)    "f" #n ,
 
 typedef enum {
-  TOKEN_VARDEF,
   TOKEN_MATHOP,
+  TOKEN_ASSIGN,
   TOKEN_BITWISE,
+  TOKEN_IDENTIF,
+  TOKEN_LIT,
+  TOKEN_KW,
+  TOKEN_PUNC,
+  TOKEN_COMPARE,
   TOKEN_EOF,
   TOKEN_UNKNOWN,
 } TOKEN_TYPE;
@@ -56,7 +61,7 @@ inline void print_help() {
 }
 
 inline bool is_newline(char c) {
-  return ( c == '\n' || c == '\r' || c == '\0');
+  return ( c == '\n' || c == '\r');
 }
 
 const char* load_file(const char *path) {
@@ -97,33 +102,13 @@ const char* load_file(const char *path) {
 
     file[fsize] = '\0';
     fclose(fp);
-    
-    char *rptr = file;
-    char *wrptr = file;
-    while (*rptr) {
-      char *lstart = rptr;
-      char *lend = strchr(rptr, '\n');
-      size_t llen = lend ? (size_t)(lend - rptr + 1) : strlen(rptr);
-
-      char *check = rptr;
-      while (check < (lstart + llen) && isspace((unsigned char)*check) && is_newline(*check)) check++;
-      bool is_blank = is_newline(*check);
-      if ( ((lstart + llen) - check) < 2 || strncmp(check, "//", 2) != 0 || is_blank) {
-	if (wrptr != rptr) memcpy(wrptr, rptr, llen);
-	wrptr += llen;
-      }
-      if (!lend) break;
-      rptr = lend + 1;
-    }
-    *wrptr = '\0';
-    char *shrunk = realloc(file, (wrptr - file) + 1);
-    return shrunk ? shrunk : file;
+    return file;
   } else {
     return NULL;
   }
 }
 
-const char *vdlist[] = {
+const char *kwlist[] = {
   SIZES(AS_UNSIGNED)
   SIZES(AS_SIGNED)
   SIZES(AS_FLOAT)
@@ -135,7 +120,17 @@ const char *vdlist[] = {
   "auto",
   "any",
   "static",
+  "if",
+  "for",
+  "while",
+  "ret",
+  "defer",
+  "else",
+  "struct",
+  "union",
+  "enum",
 };
+const size_t kwlistlen = sizeof(kwlist) / sizeof(kwlist[0]);
 
 const char *mathoplist[] = {
   "+",
@@ -143,6 +138,7 @@ const char *mathoplist[] = {
   "/",
   "*",
 };
+const size_t mathoplistlen = sizeof(mathoplist) / sizeof(mathoplist[0]);
 
 const char *bitoplist[] = {
   "^",
@@ -152,10 +148,16 @@ const char *bitoplist[] = {
   "<<",
   ">>",
 };
+const size_t bitoplistlen = sizeof(bitoplist) / sizeof(bitoplist[0]);
 
-inline bool is_vardef(char *start, unsigned int len) {
-  for (unsigned int i = 0; i < (sizeof(vdlist) / (sizeof(vdlist[0]))); i++) {
-    if (strncmp(start, vdlist[i], len) == 0) {
+const char *complist[] = {
+  "==", "!=", "<=", ">=", "<", ">"
+};
+const size_t complistlen = sizeof(complist) / sizeof(complist[0]);
+
+inline bool is_kw(char *start, unsigned int len) {
+  for (unsigned int i = 0; i < kwlistlen; i++) {
+    if (strlen(kwlist[i]) == len && strncmp(start, kwlist[i], len) == 0) {
       return true;
     }
   }
@@ -163,7 +165,7 @@ inline bool is_vardef(char *start, unsigned int len) {
 }
 
 inline bool is_mathop(char op) {
-  for (unsigned int i = 0; i < (sizeof(mathoplist) / sizeof(mathoplist[0])); i++) {
+  for (unsigned int i = 0; i < mathoplistlen; i++) {
     if (op == *mathoplist[i]) {
       return true;
     }
@@ -172,46 +174,232 @@ inline bool is_mathop(char op) {
 }
 
 inline bool is_bitopt(char *start, unsigned int len) {
-  for (unsigned int i = 0; i < (sizeof(bitoplist) / sizeof(bitoplist[0])); i++) {
-    if (strncmp(start, bitoplist[i], len) == 0) {
+  for (unsigned int i = 0; i < bitoplistlen; i++) {
+    if (strlen(bitoplist[i]) == len && strncmp(start, bitoplist[i], len) == 0) {
       return true;
     }
   }
   return false;
 }
 
+inline bool is_punc(char c) {
+    switch (c) {
+      case ',': case '{': case '}': case '(': 
+      case ')': case '[': case ']': case ';': case '.': return true;
+      default: return false;
+    }
+}
+
+inline bool is_numeric_slice(const char *start, unsigned int len) {
+    if (len == 0) return false;
+
+    unsigned int i = 0;
+    bool has_decimal = false;
+    bool has_digits = false;
+
+    if (start[0] == '-') {
+        i++;
+    }
+
+    for (; i < len; i++) {
+        if (start[i] == '.') {
+            if (has_decimal) return false;
+            has_decimal = true;
+        } else if (start[i] >= '0' && start[i] <= '9') {
+            has_digits = true;
+        } else {
+            return false;
+        }
+    }
+
+    return has_digits;
+}
+
+bool is_lit(const char *start, unsigned int len) {
+    if (len == 0) return false;
+
+    if (len >= 2 && ((start[0] == '"' && start[len - 1] == '"') || (start[0] == '\'' && start[len - 1] == '\'')) && start[len - 2] == '\\') {
+        return true;
+    }
+
+    if ((len == 4 && strncmp(start, "true", 4) == 0) ||
+        (len == 5 && strncmp(start, "false", 5) == 0)) {
+        return true;
+    }
+
+    return is_numeric_slice(start, len);
+}
+
+inline bool is_compare(char *start, unsigned int len) {
+  for (unsigned int i = 0; i < complistlen; i++) {
+    if (strlen(complist[i]) == len && strncmp(start, complist[i], len) == 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void skip_irrelevant(LexCtx *ctx) {
+  while (*ctx->curr != '\0') {
+    if (isspace(*ctx->curr)) {
+      if (is_newline(*ctx->curr)) {
+	ctx->line++;
+	ctx->col = 0;
+      } else {
+	ctx->col++;
+      }
+      ctx->curr++;
+    } else if (strncmp(ctx->curr, "//", 2) == 0) {
+      while (*ctx->curr != '\0' && !is_newline(*ctx->curr)) {
+	ctx->curr++;
+	ctx->col++;
+      }
+    } else {
+      break;
+    }
+  }
+}
+
 Token next_token(LexCtx *ctx) {
-  while (isspace(*ctx->curr) && !is_newline(*ctx->curr)) {
+  skip_irrelevant(ctx);
+
+  ctx->start = ctx->curr;
+  if (*ctx->curr == '\0') {
+    return (Token){ .start = ctx->start, .len = 0, .type = TOKEN_EOF };
+  }
+
+  TOKEN_TYPE type;
+  unsigned int len = 0;
+
+  // Identifiers, Keywords, Booleans
+  if (isalpha(*ctx->curr) || *ctx->curr == '_') {
+    while (isalnum(*ctx->curr) || *ctx->curr == '_') {
+      ctx->curr++; ctx->col++;
+    }
+    len = ctx->curr - ctx->start;
+
+    if ((len == 4 && strncmp(ctx->start, "true", 4) == 0) ||
+        (len == 5 && strncmp(ctx->start, "false", 5) == 0)) {
+      type = TOKEN_LIT;
+    } 
+    else if (is_kw(ctx->start, len)) {
+      type = TOKEN_KW;
+    } 
+    else {
+      type = TOKEN_IDENTIF;
+    }
+  } 
+  
+  // Numeric Literals
+  else if (isdigit(*ctx->curr)) {
+    bool has_dot = false;
+    while (isdigit(*ctx->curr) || *ctx->curr == '.') {
+      if (*ctx->curr == '.') {
+        if (has_dot) break;
+        has_dot = true;
+      }
+      ctx->curr++; ctx->col++;
+    }
+    len = ctx->curr - ctx->start;
+    type = TOKEN_LIT;
+  } 
+
+  // String/Char Literals
+  else if (*ctx->curr == '"' || *ctx->curr == '\'') {
+    char quote = *ctx->curr;
     ctx->curr++;
     ctx->col++;
+
+    while (*ctx->curr != '\0' && *ctx->curr != quote) {
+      if (*ctx->curr == '\\') {
+	ctx->curr++; // Skip '\'
+	ctx->col++;
+	char escape = *ctx->curr;
+	switch (escape) {
+	  case 'n': case 't': case 'r': case '\\': case '"': case '\'':
+	    // Valid (not implementing unicode and all the other ones bro gimmie a break)
+	    break;
+	  default:
+	    fprintf(stderr, "Error: Invalid escape sequence \\%c\n", escape);
+	    return (Token){ .start = ctx->start, .len = (ctx->curr - ctx->start), .type = TOKEN_UNKNOWN };
+	}
+	if (*ctx->curr == '\0') break;
+	ctx->curr++; 
+	ctx->col++;
+	continue; // Jump to start of loop to check for end of string
+      }
+
+      if (is_newline(*ctx->curr)) {
+	ctx->line++;
+	ctx->col = 0; 
+      } else {
+	ctx->col++;
+      }
+      ctx->curr++;
+    }
+
+    if (*ctx->curr == '\0') {
+      printf("Error: Unterminated string at line %u col %u\n", ctx->line, ctx->col);
+      return (Token){ .start = ctx->start, .len = (ctx->curr - ctx->start), .type = TOKEN_UNKNOWN };
+    } else {
+      if (quote == '\'' && (ctx->curr - ctx->start) > 3) {
+	fprintf(stderr, "Char literal must contain only 1 char");
+	return (Token){ .start = ctx->start, .len = (ctx->curr - ctx->start), .type = TOKEN_UNKNOWN};
+      }
+      ctx->curr++; 
+      ctx->col++;
+      len = ctx->curr - ctx->start;
+      type = TOKEN_LIT;
+    }
   }
-  if (is_newline(*ctx->curr)) ctx->line++;
-  ctx->start = ctx->curr;
-  while (isalnum(*ctx->curr)) ctx->curr++;
-  char *token_end = ctx->curr - 1;
-  unsigned int len = token_end - ctx->start;
-  TOKEN_TYPE type;
-  if (*ctx->curr == '\0') {
-    type = TOKEN_EOF;
-  } else if (is_vardef(ctx->start, len)) {
-    type = TOKEN_VARDEF;
-  } else if (is_mathop(*token_end)) {
-    type = TOKEN_MATHOP;
-  } else if (is_bitopt(ctx->start, len)) {
-    type = TOKEN_BITWISE;
-  } else {
-    type = TOKEN_UNKNOWN;
+
+  // Operators and Punctuation
+  else {
+    if (is_compare(ctx->curr, 2) || is_bitopt(ctx->curr, 2)) {
+      type = is_compare(ctx->curr, 2) ? TOKEN_COMPARE : TOKEN_BITWISE;
+      len = 2;
+    } else if (*ctx->curr == '=') {
+      type = TOKEN_ASSIGN;
+      len = 1;
+    } else if (is_compare(ctx->curr, 1) || is_bitopt(ctx->curr, 1) || is_mathop(*ctx->curr) || is_punc(*ctx->curr)) {
+      len = 1;
+      if (is_compare(ctx->curr, 1)) type = TOKEN_COMPARE;
+      else if (is_bitopt(ctx->curr, 1)) type = TOKEN_BITWISE;
+      else if (is_mathop(*ctx->curr)) type = TOKEN_MATHOP;
+      else if (is_punc(*ctx->curr)) type = TOKEN_PUNC;
+      else type = TOKEN_UNKNOWN;
+    } else {
+      type = TOKEN_UNKNOWN;
+      len = 1;
+    }
+
+    ctx->curr += len;
+    ctx->col += len;
   }
-  Token t = {
-    .start = ctx->start,
-    .len = len,
-    .type = type,
-  };
-  return t;
+
+  return (Token){ .start = ctx->start, .len = len, .type = type };
+}
+
+Token peek_token(LexCtx *ctx) {
+    char *saved_curr = ctx->curr;
+    unsigned int saved_line = ctx->line;
+    unsigned int saved_col = ctx->col;
+
+    Token t = next_token(ctx);
+
+    ctx->curr = saved_curr;
+    ctx->line = saved_line;
+    ctx->col = saved_col;
+
+    return t;
 }
 
 void try_compile(const char *path) {
   const char *file = load_file(path);
+  if (!file) {
+    fprintf(stderr, "Failed to load file: %s", path);
+    exit(1);
+  }
   LexCtx *ctx = malloc(sizeof(LexCtx));
   if (!ctx) {
     fprintf(stderr, "Failed to malloc a lex ctx");
@@ -224,8 +412,56 @@ void try_compile(const char *path) {
   ctx->col = 1;
   Token t;
   while ((t = next_token(ctx)).type != TOKEN_EOF) {
-    // Figure out what to do with tokens
+    switch (t.type) {
+      case TOKEN_UNKNOWN: {
+	fprintf(stderr, "Encountered unknown token %.*s at line %u, col %u", t.len, t.start, ctx->line, ctx->col);
+	goto err_cleanup;
+      }
+      case TOKEN_BITWISE: {
+
+	break;
+      }
+      case TOKEN_MATHOP: {
+
+	break;
+      }
+      case TOKEN_ASSIGN: {
+
+	break;
+      }
+      case TOKEN_PUNC: {
+
+	break;
+      }
+      case TOKEN_IDENTIF: {
+
+	break;
+      }
+      case TOKEN_LIT: {
+
+	break;
+      }
+      case TOKEN_KW: {
+
+	break;
+      }
+      case TOKEN_COMPARE: {
+
+	break;
+      }
+      case TOKEN_EOF: {
+	fprintf(stderr, "Encounted unexpected EOF"); // Should never happen
+	goto err_cleanup;
+      }
+    }
   }
+  free((void*)file);
+  free(ctx);
+  return;
+err_cleanup:
+  free((void*)file);
+  free(ctx);
+  exit(127);
 }
 
 int main(int argc, char **argv) {
@@ -233,13 +469,21 @@ int main(int argc, char **argv) {
     print_help();
     exit(1);
   }
-  for (unsigned i = 0; i < (unsigned int)argc; i++) {
+
+  bool found_file = false;
+  for (unsigned i = 1; i < (unsigned int)argc; i++) {
     if (check_exists(argv[i])) {
-      printf("A valid file was found, attempting to compile it");
+      found_file = true;
+      printf("Compiling %s\n", argv[i]);
       try_compile(argv[i]);
     }
   }
-  printf("No valid file found");
-  print_help();
-  return 1;
+
+  if (!found_file) {
+    printf("No valid file found\n");
+    print_help();
+    return 1;
+  }
+
+  return 0;
 }
